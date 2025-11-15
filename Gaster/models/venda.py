8440901c4adbbd9ba92435ffab952ef2d45f19e5
Raquel <raquelmartiniano14@gmail.com>
2025-11-15 22:04:03 +0000

    outra correção na venda

--- Gaster/models/venda.py
+++ Gaster/models/venda.py
@@ -3,32 +3,32 @@ from models.vendaitem import VendaItem, VendaItemDAO
 from models.cliente import ClienteDAO
 from datetime import datetime
 class Venda:
-    def __init__(self, id):
+    def __init__(self, id, id_Cliente):
         self.set_id(id)
-        self.set_data()
+        self.set_data(None)
         self.set_carrinho()
         self.set_total()
-        self.set_id_Cliente()
+        self.set_id_Cliente(id_Cliente)
     
     #Set
     def set_id(self, id):
         self.id = id
-    def set_data(self):
-        self.data = datetime.now()
+    def set_data(self, data):
+        if data is None:
+            self.data = datetime.now()
+        else:
+            self.data = data
     def set_carrinho(self):
         self.carrinho = True
     def set_total(self):
         total = 0
         for obj in VendaItemDAO.listar():
-            if obj.get_idVenda == self.id:
-                total = total + (obj.get_preco() * obj.get_qtd())
+            if obj.get_idVenda() == self.id:
+                total += obj.get_preco() * obj.get_qtd()
         self.total = total
          
-    def set_id_Cliente(self):
-        self.id_Cliente = None
-        # for obj in ClienteDAO.listar():
-        #     if obt.get_id() == id_Cliente: self.id_Cliente = id_Cliente
-    
+    def set_id_Cliente(self, id_Cliente):
+        self.id_Cliente = id_Cliente
     #Get
     def get_id(self): return self.id
     def get_data(self): return self.data
@@ -38,11 +38,13 @@ class Venda:
     
     def cliente_nome(self):
         for obj in ClienteDAO.listar():
-            if obj.get_id() == self.id_Cliente: return obj.get_nome()
+            if obj.get_id() == self.id_Cliente: 
+                return obj.get_nome() 
+        return 'Cliente não encontrado'
 
 
     def __str__(self):
-        return f"{self.id} - {self.data.strftime("%d/%m/%Y")} - {self.carrinho} - {self.total} - {self.id_Cliente}"
+        return f"{self.id} - {self.data.strftime('%d/%m/%Y')} - {self.carrinho} - {self.total} - {self.id_Cliente}"
     def to_json(self):
         return { "id" : self.id, "data" : self.data.strftime("%d/%m/%Y"), "total" : self.total, "id_Cliente" : self.id_Cliente }
     def from_json(dic):
@@ -80,6 +82,7 @@ class VendaDAO:
             cls.objetos.append(obj)
             cls.salvar()
     
+    @classmethod
     def excluir(cls, obj):
         aux = cls.lista_id(obj.id)
         if aux != None:
