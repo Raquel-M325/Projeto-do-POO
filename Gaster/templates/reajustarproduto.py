b472aca9612b6a469ab726059d957f168f2a5ff6
Migstand <mrsdahora@gmail.com>
2025-12-03 01:27:37 +0000

    Rejustar Produto

--- Gaster/templates/reajustarproduto.py
+++ Gaster/templates/reajustarproduto.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import pandas as pd
+from view import View
+import time
 
 class ReajustarProdutoUI:
     def main():
         st.header("Reajustar Preços de Produtos")
+
+    def reajustar():
+        preco = text_input("Informe o preço a ser reajustado")
+        if st.button("Reajustar"):
+            View.reajustar_preco(preco)
+            st.success("Preço atualizado com sucesso")
+            time.sleep(2)
+            st.rerun()
