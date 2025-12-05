fde0f0be5f2f9d2270d462e889d91afb91dce08f
Raquel <raquelmartiniano14@gmail.com>
2025-12-05 02:17:30 +0000

    tentando tirar o conflito

--- Gaster/templates/confirmarcompra.py
+++ Gaster/templates/confirmarcompra.py
@@ -5,6 +5,11 @@ import pandas as pd
 class ConfirmarCarrinhoUI:
     def main():
         st.header("Confirmação de Compra")
-        if View.comprar_carrinho() != None: print("Carrinho confirmado! Compra com sucesso!")
-        else:
-            print("Compra não foi executada, ainda há carrinho com os produtos")
+        st.session_state["cliente_id"] = c["id"]
+        pagar = st.text_input("Informe a opção do pagamento: ")
+        if st.button("Confirmar"):
+            if View.opcao_pagar(pagar) != None: 
+                st.write("Carrinho confirmado! Compra com sucesso!")
+            else:
+                st.write("Compra não foi executada, ainda há carrinho com os produtos")
+                st.rerun()
