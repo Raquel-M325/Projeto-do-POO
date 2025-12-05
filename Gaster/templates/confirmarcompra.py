4b6ba815a0d6634744d0f6e3f3429483bfac8a4c
Raquel <raquelmartiniano14@gmail.com>
2025-12-05 01:29:44 +0000

    fazendo do confirmar compra

--- Gaster/templates/confirmarcompra.py
+++ Gaster/templates/confirmarcompra.py
@@ -4,11 +4,7 @@ import pandas as pd
 
 class ConfirmarCarrinhoUI:
     def main():
-        st.header("")
-        try:
-            ConfirmarCarrinhoUI.listar()
-        except:
-
-
-
-    def
+        st.header("Confirmação de Compra")
+        if View.comprar_carrinho() != None: print("Carrinho confirmado! Compra com sucesso!")
+        else:
+            print("Compra não foi executada, ainda há carrinho com os produtos")
