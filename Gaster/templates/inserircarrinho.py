2adea47b88fe2e03705cac28ce16f7ec46dca0d4
Migstand <mrsdahora@gmail.com>
2025-12-05 01:15:59 +0000

    inserir carrinho

---
+++ Gaster/templates/inserircarrinho.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import time
+from view import View
+import pandas as pd
+
+class InserirCarrinhoUI:
+    def main():
+        st.header("Insira seu item no carrinho")
+        tab1 = (["Inserir"])
+
+    def inserir():
+        venda = View.venda_existe(st.session_state["cliente_id"])
+        InserirCarrinhoUI.listar()
+        produto = st.number_input("Digite o id do seu produto")
+        quantidade = st.number_input("Diga quantos você quer")
+        preco = View.achar_preco(produto)
+        View.atualizar_estoque(produto, quantos, venda, preco)
+    
+    def listar():
+        produto = View.produto_listar()
+        if len(produto) == 0: st.write("Nenhum Produto cadastrado")
+        else:
+            list_dic = []
+            for obj in produto: list_dic.append(obj.to_json())
+            df = pd.DataFrame(list_dic)
+            st.dataframe(df, hide_index=True, column_order=["descricao", "preco", "estoque"])
