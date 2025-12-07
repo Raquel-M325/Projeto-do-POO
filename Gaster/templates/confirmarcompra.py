980bd45bf1c41140469a41baa8af2cd9cca7d27e
Raquel <raquelmartiniano14@gmail.com>
2025-12-07 19:54:07 +0000

    buscando corrigir mais bugs

--- Gaster/templates/confirmarcompra.py
+++ Gaster/templates/confirmarcompra.py
@@ -5,16 +5,25 @@ import pandas as pd
 class ConfirmarCarrinhoUI:
     def main():
         st.header("Confirmação de Compra")
-        pagar = st.text_input("Informe a opção de pagamento:")
+        pagar = st.number_input("Informe a opção de pagamento:")
 
         if st.button("Confirmar"):
             verificacao = View.opcao_pagar(pagar)
-
             if verificacao is not None:
-                st.session_state["usuario_id"] = verificacao["id"] #acho que nessa parte esteja incompleta
-                st.success("Carrinho confirmado! Compra com sucesso!")
-                st.rerun()
+                if verificacao == 1:
+                    credito = View.pagamento_credito()  
+                
+                if verificacao == 2:
+                    debito = View.pagamento_debito()
 
-            else:
-                st.error("Compra não foi executada. Ainda há produtos no carrinho.")
-                st.rerun()
+                if verificacao == 3:
+                    pix = View.pagamento_pix()
+
+            finalizacao = View.finalizacao()
+            st.session_state["usuario_id"] = finalizacao["id"] #acho que nessa parte esteja incompleta
+            st.success("Carrinho confirmado! Compra com sucesso!")
+            st.rerun()
+
+        else:
+            st.error("Compra não foi executada. Ainda há produtos no carrinho.")
+            st.rerun()
